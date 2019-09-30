761c9fddfe38d70643355ae5102284946689ccbc
Senki <aszitymartin@gmail.com>
2019-09-30 23:56:02 +0200

    one rotation done

--- hilbertcurveasdatastructure.py
+++ hilbertcurveasdatastructure.py
@@ -1,3 +1,5 @@
+from collections.abc import Iterable
+
 class point:
     def __init__(self, vertical, horizontal):
         self.vertical = vertical
@@ -8,19 +10,19 @@ class point:
 
 
 # refactor it as a class i guess
-def create_hilbert_0(a, b,*args,**kvargs):
+def create_hilbert_0(a, b, *args, **kvargs):
     hilbert0 = [point(0 + a, 0 + b), point(0 + a, 1 + b), point(1 + a, 1 + b), point(1 + a, 0 + b)]
     return hilbert0
 
 
 # find a way to rotate the first counter clockwise and the last clockwise
-def hilbert_nonzero(a, b, n,*args,**kvargs):
+def hilbert_nonzero(a, b, n, *args, **kvargs):
     if n == 0:
         list_of_hilbert = create_hilbert_0(0 + a, 0 + b)
 
     else:
         list_of_hilbert = [
-            hilbert_nonzero(a + 0 * n, b + 0 * n, n - 1),
+            amiright(hilbert_nonzero(a + 0 * n, b + 0 * n, n - 1)),
             hilbert_nonzero(a + 0 * n, b + 2 * n, n - 1),
             hilbert_nonzero(a + 2 * n, b + 2 * n, n - 1),
             hilbert_nonzero(a + 2 * n, b + 0 * n, n - 1)]
@@ -28,15 +30,22 @@ def hilbert_nonzero(a, b, n,*args,**kvargs):
     return list_of_hilbert
 
 
-def rotate90(*args,**kvargs):
-    pass
+# the following should change the x=y y=x that's it nothing harder than this this only need to just do that
+def amiright(maybe_array):
+    lista = []
+    if isinstance(maybe_array, Iterable):
+        for element in maybe_array:
+             lista.append(amiright(element))
+    else:
+        return mirror(maybe_array)
+    return lista
 
 
-def rotate270(*args,**kvargs):
-    pass
+def mirror(maybe_array):
+    return point(maybe_array.horizontal,maybe_array.vertical)
 
 
-def main(*args,**kvargs):
+def main(*args, **kvargs):
     if __name__ == "__main__":
         marray = hilbert_nonzero(*args)
         print("final one: %s" % marray)

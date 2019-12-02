e6c1ce129638fc9c9ee2d2f8825e5979d0d2f4ca
5enk1 <aszitymartin@gmail.com>
2019-12-02 19:28:31 +0100

    better naming - if else

--- adventofcode/adventofcode2.py
+++ adventofcode/adventofcode2.py
@@ -13,20 +13,25 @@ class opcode():
 
 
 def calculate(noun, verb):
-    return (100 * noun) + verb
+    return 100 * noun + verb
 
 
-def main(Intcode: list):
+def int_code_parser(Intcode: list):
+    def if_one(a, b, c
+               ): Intcode[a] = Intcode[b] + Intcode[c]
+    def if_two(a, b, c
+               ): Intcode[a] = Intcode[b] * Intcode[c]
+
+    def if_anything_else(a, b, c): pass
+
+    options = {1: if_one, 2: if_two}
     x = 0
     while (Intcode[0+x] != 99):
-        current = opcode(Intcode[0+x], Intcode[1+x], Intcode[2+x], Intcode[3+x])
-        if current.what_to_do == 1:
-            Intcode[current.what_to_change] = Intcode[current.pos1] + Intcode[current.pos2]
-        elif current.what_to_do == 2:
-            Intcode[current.what_to_change] = Intcode[current.pos1] * Intcode[current.pos2]
-        else:
-            pass
+        current = opcode(Intcode[0+x], Intcode[1+x],
+                         Intcode[2+x], Intcode[3+x])
 
+        options.get(
+            Intcode[0+x], if_anything_else)(current.what_to_change, current.pos1, current.pos2)
         x += 4
     return(Intcode)
 
@@ -37,12 +42,11 @@ def doubleloop(Intcode: list, calculate_methode):
         for k in range(0, 100):
             originalCode = copy.copy(Intcode)
             originalCode[1], originalCode[2] = i, k
-            result = main(originalCode)[0]
+            result = int_code_parser(originalCode)[0]
             if result == 19690720:
                 return calculate_methode(i, k)
 
 
 if __name__ == "__main__":
-    # test([2,4,4,5,99,0])
     print(doubleloop([1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 10, 1, 19, 2, 19, 6, 23, 2, 13, 23, 27, 1, 9, 27, 31, 2, 31, 9, 35, 1, 6, 35, 39, 2, 10, 39, 43, 1, 5, 43, 47, 1, 5, 47, 51, 2, 51, 6, 55, 2, 10, 55, 59, 1, 59,
                       9, 63, 2, 13, 63, 67, 1, 10, 67, 71, 1, 71, 5, 75, 1, 75, 6, 79, 1, 10, 79, 83, 1, 5, 83, 87, 1, 5, 87, 91, 2, 91, 6, 95, 2, 6, 95, 99, 2, 10, 99, 103, 1, 103, 5, 107, 1, 2, 107, 111, 1, 6, 111, 0, 99, 2, 14, 0, 0], calculate))

6330d6e896953d72fccd2a7ce27acd1a91cab787
Senki <aszitymartin@gmail.com>
2019-12-28 13:28:35 +0100

    delete button

--- angular-ani/ani/src/app/anime/anime-list.component.ts
+++ angular-ani/ani/src/app/anime/anime-list.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { Component } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-anime-list',
-  templateUrl: 'anime-list.component.html'
+  selector: "app-anime-list",
+  templateUrl: "anime-list.component.html"
 })
 export class AnimeListComponent {
-  title = 'anime list';
-  animes: any[];
+  title = "anime list";
+  tests: Observable<any[]>;
+  tests2: any;
+  db: any;
 
   constructor(db: AngularFirestore) {
-    db.collection('anime')
-      .valueChanges()
-      .subscribe(anime => (this.animes = anime));
+    this.db = db;
+    this.tests = db.collection("anime").valueChanges();
+    this.tests2 = db.collection("anime").snapshotChanges();
+  }
+
+  deleteAnime(awd: any) {
+    console.log(awd.payload.doc.id);
+    this.db.collection('anime').doc(awd.payload.doc.id).delete();
   }
 }
